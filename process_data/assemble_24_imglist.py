50d1edda13b0fd1241956debc1db56bcdba2b583
akinoriosamura <osamura.akinori@gmail.com>
2019-07-26 19:27:48 +0900

    create 24 imglist script

--- process_data/assemble_24_imglist.py
+++ process_data/assemble_24_imglist.py
@@ -3,10 +3,10 @@ import sys
 sys.path.append(os.getcwd())
 import process_data.assemble as assemble
 
-pnet_postive_file = './dataset/anno_store/pos_12.txt'
-pnet_part_file = './dataset/anno_store/part_12.txt'
-pnet_neg_file = './dataset/anno_store/neg_12.txt'
-imglist_filename = './dataset/anno_store/imglist_anno_12.txt'
+pnet_postive_file = './dataset/anno_store/pos_24.txt'
+pnet_part_file = './dataset/anno_store/part_24.txt'
+pnet_neg_file = './dataset/anno_store/neg_24.txt'
+imglist_filename = './dataset/anno_store/imglist_anno_24.txt'
 
 if __name__ == '__main__':
 
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     anno_list.append(pnet_neg_file)
 
     chose_count = assemble.assemble_data(imglist_filename ,anno_list)
-    print("PNet train annotation result file path:%s" % imglist_filename)
+    print("train annotation result file path:%s" % imglist_filename)
     print("data num: ", chose_count)
